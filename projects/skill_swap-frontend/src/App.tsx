23e7ca043e4e48aac965dbcce1c6aff2dc21149e
RAM CHARAN TEJA <135548573+Ramcharan1706@users.noreply.github.com>
2025-10-30 07:37:15 +0530

    Remove WalletConnect v2 integration comments
    Removed comments regarding WalletConnect v2 integration.

--- projects/skill_swap-frontend/src/App.tsx
+++ projects/skill_swap-frontend/src/App.tsx
@@ -21,8 +21,6 @@ if (import.meta.env.VITE_ALGOD_NETWORK === 'localnet') {
     { id: WalletId.DEFLY },
     { id: WalletId.PERA },
     { id: WalletId.EXODUS },
-    // If you are interested in WalletConnect v2 provider
-    // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
   ]
 }
 
