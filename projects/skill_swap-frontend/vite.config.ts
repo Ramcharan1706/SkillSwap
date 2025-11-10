ba518fe0ab4d0e5d0020b6cb291b15327b78ebb6
RAM CHARAN TEJA <135548573+Ramcharan1706@users.noreply.github.com>
2025-11-10 19:38:47 +0530

    Update vite.config.ts

--- projects/skill_swap-frontend/vite.config.ts
+++ projects/skill_swap-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,9 @@ export default defineConfig({
     }),
   ],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
